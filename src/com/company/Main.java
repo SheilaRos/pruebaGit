581d3d426e7891c8758e33b3252d5e03f9715794
SheilaRos <sheila.ros.cheme@gmail.com>
2017-02-17 20:42:54 +0100

    primera version Task #1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(17);
+        numbers.add(5);
+        numbers.add(30);
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
         // write your code here
     }
 }
